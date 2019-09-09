68131f3d845ffb8116f8d969d28178e0a47d67b0
brezzz <brezzz@users.noreply.github.com>
2019-09-09 18:26:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-
-const apiKey = 'q7Gu44bcxnKu9SLzeeRui7Rc7iUXI2LWfbr1jsAt'
+const apiKey = 'q7Gu44bcxnKu9SLzeeRui7Rc7iUXI2LWfbr1jsAt';
 
 const searchURL = 'https://developer.nps.gov/api/v1/parks';
-
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
@@ -13,17 +11,15 @@ function formatQueryParams(params) {
  console.log('queryItems is: '+ queryItems);
 }
 
-
-
 function displayResults(responseJson, limitinput) {
   console.log(responseJson);
+  $('.loading-message').addClass('hidden');
   $('#results-list').empty();
- 
+  
   for (let i = 0; i < responseJson.data.length & i<limitinput ; i++){
     
-    
-    
-    let address =  `${responseJson.data[i].addresses[0].line1}, 
+    let address =  
+   ` ${responseJson.data[i].addresses[0].line1}
     ${responseJson.data[i].addresses[0].line2}
     ${responseJson.data[i].addresses[0].line3}
     ${responseJson.data[i].addresses[0].city}, 
@@ -32,16 +28,19 @@ function displayResults(responseJson, limitinput) {
   
 
   $('#results-list').append(
-       `<li><h3><a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></h3>
-      <p>${responseJson.data[i].fullName}</p>
-      <p>Description: ${responseJson.data[i].description}</p>
-     
-    <p> Address: ${address} </p>
+       `<li>
+      <h1>${responseJson.data[i].fullName}</h1>
+      <img src='${responseJson.data[i].images[0].url}' alt='${responseJson.data[i].images[0].altText}'>
+     <h5>${responseJson.data[i].images[0].altText}</h5> <h6>(Credit: ${responseJson.data[i].images[0].credit})</h6>
+     <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a>
+
+    <p><span class ='bold'>Description</span>: ${responseJson.data[i].description}</p> 
+    <p><span class ='bold'>Address:</span> ${address} </p>
+    <p><span class ='bold'>Weather: </span>${responseJson.data[i].weatherInfo}</p>
+    <span class ='bold caption'>${responseJson.data[i].images[0].caption}</span>
     </li>`
 
-    
-
-    
+  
     )};
   //display the results section  
   $('#results').removeClass('hidden');
@@ -51,7 +50,7 @@ function getNews(stateCodeinput, limitinput=10) {
   const params = {
     stateCode: stateCodeinput,
    limit: limitinput,
-   fields:'addresses',
+   fields:'addresses,images',
     api_key:apiKey,
   };
   const queryString = formatQueryParams(params)
@@ -81,10 +80,13 @@ $('#results-list').html("");
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
+    
+    $('.loading-message').removeClass('hidden');
+    // setTimeout(function(){ $('.loading-message').removeClass('hidden'); }, 1500); 
     const stateCodeinput = $('#js-search-state').val();
     const limitinput = $('#js-results-limit').val();
     getNews(stateCodeinput, limitinput);
   });
 }
 
-$(watchForm);
+$(watchForm)
